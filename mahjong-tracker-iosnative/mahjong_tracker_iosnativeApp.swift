c2f14a10299fa46d034287c528a01e4aaa62afa6
ryota honma <honma.chuo55@gmail.com>
2026-03-14 23:35:58 +0900

    Initial Commit

---
+++ mahjong-tracker-iosnative/mahjong_tracker_iosnativeApp.swift
@@ -0,0 +1,32 @@
+//
+//  mahjong_tracker_iosnativeApp.swift
+//  mahjong-tracker-iosnative
+//
+//  Created by 本間諒太 on 2026/03/14.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct mahjong_tracker_iosnativeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
